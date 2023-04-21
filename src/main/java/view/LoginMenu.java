d8c170599322d9acee8f19ccda53c019da97eb21
M-H-Boroumandnia <118667895+M-H-Boroumandnia@users.noreply.github.com>
2023-04-22 02:41:15 +0330

    command parser updated and bugs fixed

--- src/main/java/view/LoginMenu.java
+++ src/main/java/view/LoginMenu.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 public class LoginMenu {
     private Controller controller;
-    private CommandParser commandParser;
+    private final CommandParser commandParser;
     public LoginMenu (Controller controller) {
         this.controller = controller;
         commandParser = new CommandParser();
@@ -18,9 +18,10 @@ public class LoginMenu {
         String command;
         while (true) {
             command = CommandParser.getScanner().nextLine();
-            if (command.matches("^exit$")) return "exit";
-            if ((optionPass = commandParser.validate(command,"user create","u|username/p|password/n|nickname/e|email/?s|slogan")) != null)
+            if (commandParser.validate(command,"exit",null) != null) return "exit";
+            if ((optionPass = commandParser.validate(command,"user create","u|username/?p|password/n|nickname/e|email/s|slogan")) != null)
                 System.out.println(Controller.createUser(optionPass));
+            else System.out.println("Invalid command!");
         }
     }
 }
