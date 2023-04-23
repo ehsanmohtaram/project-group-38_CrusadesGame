135ea11a367d21e87c95af0234d164c014d5b759
M-H-Boroumandnia <118667895+M-H-Boroumandnia@users.noreply.github.com>
2023-04-23 21:06:38 +0330

    updated merge

--- src/main/java/controller/MapDesignController.java
+++ src/main/java/controller/MapDesignController.java
@@ -1,25 +1,19 @@
 package controller;
 
 import model.Map;
-import model.MapBlock;
-import model.MapBlockType;
-import view.DesignMapMenu;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.regex.Matcher;
+import view.SelectMapMenu;
 
 public class MapDesignController {
     private Map gameMap;
-    private DesignMapMenu designMapMenu;
+    private SelectMapMenu selectMapMenu;
 
     public MapDesignController(Map gameMap) {
         this.gameMap = gameMap;
-        designMapMenu = new DesignMapMenu(this);
+        selectMapMenu = new SelectMapMenu(this);
     }
 
     public void run(){
-        switch (designMapMenu.run()){
+        switch (selectMapMenu.run()){
             case "start":
                 GameController gameController = new GameController(gameMap);
                 gameController.run();
@@ -28,46 +22,4 @@ public class MapDesignController {
                 return;
         }
     }
-
-    public String setTexture(HashMap<String , String> options) {
-        MapBlockType mapBlockType;
-        if((mapBlockType = MapBlock.findEnumByLandType(options.get("t"))) == null)
-            return "no such type available for lands";
-        if(options.get("x") == null && options.get("y") == null) {
-            ArrayList<Integer> bounds = new ArrayList<>();
-            options.remove("x");
-            options.remove("y");
-            for (String key : options.keySet()) {
-                if (!key.equals("n") && !options.get(key).matches("\\d+"))
-                    return "please choose a digit for bounds";
-                else if(!key.equals("n"))
-                    bounds.add(Integer.parseInt(options.get(key)));
-            }
-            gameMap.changeType(bounds.get(0), bounds.get(1), bounds.get(2), bounds.get(3), mapBlockType);
-        }else if(options.get("x") != null && options.get("y") != null){
-            if(!options.get("x").matches("\\d+") || !options.get("y").matches("\\d+"))
-                return "please choose digits for bounds";
-            int x = Integer.parseInt(options.get("x"));
-            int y = Integer.parseInt(options.get("y"));
-            options.remove("x");
-            options.remove("y");
-            for (String key : options.keySet())
-                if (!key.equals("n") && options.get(key) != null)
-                    return "choose two or four digits to specify area!";
-            gameMap.changeType(x, y, mapBlockType);
-        }else
-            return "you must choose at least two digits for bounds";
-        return "type changed successfully";
-    }
-
-    public String clear(HashMap<String , String> options) {
-        return null;
-    }
-    public String dropRock(HashMap<String , String> options) {
-        return null;
-    }
-    public String dropTree(HashMap<String , String> options) {
-        return null;
-    }
-
 }
--- src/main/java/view/SelectMapMenu.java
+++ src/main/java/view/SelectMapMenu.java
@@ -2,18 +2,19 @@ package view;
 
 import controller.CommandParser;
 import controller.Controller;
+import controller.GameController;
 import controller.MapDesignController;
+import model.Map;
 
-import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
-public class DesignMapMenu {
+public class SelectMapMenu {
     private Controller controller;
     private MapDesignController mapDesignController;
     private final CommandParser commandParser;
 
-    public DesignMapMenu(MapDesignController controller) {
+    public SelectMapMenu(MapDesignController controller) {
         this.mapDesignController = controller;
         commandParser = new CommandParser();
     }
@@ -21,17 +22,14 @@ public class DesignMapMenu {
     public String run(){
         System.out.println("here you can design your map. when your map is ready type \'start\'");
         Scanner scanner = CommandParser.getScanner();
-        HashMap<String , String> options;
         String input;
         Matcher matcher;
         while (true) {
             input = scanner.nextLine();
-            if ((options = commandParser.validate(input,"set texture",
-                "x|positionX/y|positionY/x1|positionX1/y1|positionY1/x2|positionX2/y2|positionY2/n|name")) != null){
-                System.out.println(mapDesignController.setTexture(options));
-                return "";
-            }else if(input.equals("start"))
-                return "start";
+            if (commandParser.validate(input,"logout",null) != null) {
+                System.out.println("User logged out successfully!");
+                return"";
+            }
             else
                 System.out.println("invalid command");
         }
